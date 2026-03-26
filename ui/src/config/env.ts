a5336b7bfe708427e691c1bd0087fa3afd817660
webuser-1109 <m88771109@gmail.com>
2026-03-27 03:02:58 +0500

    changed api proxy

--- ui/src/config/env.ts
+++ ui/src/config/env.ts
@@ -1,2 +1,3 @@
 export const API_URL = import.meta.env.VITE_API_URL;
+export const API = import.meta.env.VITE_API;
 export const ASSETS_URL = import.meta.env.VITE_ASSETS_URL;
--- ui/vite.config.ts
+++ ui/vite.config.ts
@@ -2,6 +2,7 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
 import path from "path";
+import { API } from "./src/config/env";
 
 // https://vite.dev/config/
 export default defineConfig({
@@ -14,7 +15,7 @@ export default defineConfig({
   server: {
     proxy: {
       "/api": {
-        target: "http://localhost:4000",
+        target: API,
         changeOrigin: true,
       },
     },
