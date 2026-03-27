8f7eab9878842177dd619f8f667f90a956142589
webuser-1109 <m88771109@gmail.com>
2026-03-27 09:16:46 +0500

    removed ui

--- ui/src/api/category.ts
+++
@@ -1,8 +0,0 @@
-import { axiosClient } from "@/lib/axiosClient";
-import type { CategoryTypes } from "@/types/global";
-
-export const getCategory = async (): Promise<{
-  success: boolean;
-  count: number;
-  data: CategoryTypes[];
-}> => (await axiosClient.get("/category")).data;
